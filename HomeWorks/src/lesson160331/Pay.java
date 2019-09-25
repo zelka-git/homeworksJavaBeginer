d6b2dbfd5da1c98aacd88accd59462d54496fce0
AnzhelikaEO <anzelikasht@gmail.com>
2019-09-25 17:33:27 +0300

    add Pay

---
+++ HomeWorks/src/lesson160331/Pay.java
@@ -0,0 +1,9 @@
+package lesson160331;
+
+public class Pay {
+	public static void main(String[] args) {
+		int dirtyPay = 30000;
+		double clearPay = 0.87 * dirtyPay;
+		System.out.println(clearPay);
+	}
+}
